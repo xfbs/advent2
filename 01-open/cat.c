a8a5bf767e0fb2be58d3e3e8d823a8630d899edb
Patrick Elsen <pelsen@xfbs.net>
2022-12-04 19:48:50 -0500

    Solves 01

--- 01-open/cat.c
+++ 01-open/cat.c
@@ -4,15 +4,41 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    // For cat, we have to iterate over all command-line arguments of
-    // our process. Thereby, argv[0] is our program binary itself ("./cat").
-    int idx;
-    for (idx = 1; idx < argc; idx++) {
-        printf("argv[%d] = %s\n", idx, argv[idx]);
+    char buffer[4096];
+
+    // iterate through command-line arguments (starting at argv[1]).
+    for (int i = 1; i < argc; i++) {
+        // open file
+        int fd = open(argv[i], O_RDONLY);
+        if(fd < 0) {
+            perror("open");
+            return EXIT_FAILURE;
+        }
+
+        // keep reading until EOF
+        int bytes;
+        while((bytes = read(fd, &buffer[0], sizeof(buffer))) > 0) {
+            if(write(1, &buffer[0], bytes) < 0) {
+                perror("write");
+                return EXIT_FAILURE;
+            }
+        }
+
+        // handle read error
+        if(bytes < 0) {
+            perror("read");
+            return EXIT_FAILURE;
+        }
+
+        // handle close error
+        if(close(fd) != 0) {
+            perror("close");
+            return EXIT_FAILURE;
+        }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
